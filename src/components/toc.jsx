208b0323e0929c5b97441e1bb26a048ce96d56b8
Josix <wilson8507@gmail.com>
2020-10-05 18:37:23 +0800

    Fix style of toc

--- src/components/toc.jsx
+++ src/components/toc.jsx
@@ -31,6 +31,11 @@ const styles = {
       max-height: 30vh;
     }
   `,
+  ul: {
+    listStyleType: "none",
+    marginBottom: 0,
+    marginLeft: 0,
+  },
   item: {
     lineHeight: 1.2,
     marginTop: 3,
@@ -43,7 +48,6 @@ const styles = {
   `,
   subitem: {
     fontSize: 14.5,
-    marginLeft: 25,
     listStyleType: "none",
     lineHeight: 1.2,
     marginTop: 12,
@@ -61,24 +65,32 @@ const TOC = ({
       css={styles.toc}
     >
       <h3 style={styles.h3}>Table of Contents</h3>
-      {items.map(
-        item => (
-          <li style={styles.item} key={item.url}>
-            <a css={styles.link} href={item.url} key={item.url}>
-              {item.title}
-            </a>
-            {item.items && item.items.length > 0 ? item.items.map(
-              subitem => (
-                <li style={styles.subitem} key={subitem.url}>
-                  <a css={styles.link} href={subitem.url} key={subitem.url}>
-                    {subitem.title}
-                  </a>
-                </li>
-              )
-            ) : null}
-          </li>
-        )
-      )}
+      <ul style={styles.ul}>
+        {
+          items.map(item => (
+            <li style={styles.item} key={item.url}>
+              <a css={styles.link} href={item.url} key={item.url}>
+                {item.title}
+              </a>
+              {item.items && item.items.length > 0 ? (
+                <ul style={{ ...styles.ul, marginLeft: "1.25rem" }}>
+                  {
+                    item.items.map(
+                      subitem => (
+                        <li style={styles.subitem} key={subitem.url}>
+                          <a css={styles.link} href={subitem.url} key={subitem.url}>
+                            {subitem.title}
+                          </a>
+                        </li>
+                      )
+                    )
+                  }
+                </ul>)
+              : null}
+            </li>)
+          )
+        }
+      </ul>
     </div>
   );
 }
