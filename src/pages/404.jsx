8841e2cad51c69d5bd1165f653b2c9c72a150b32
Josix <josixwang@gmail.com>
2022-08-29 00:09:11 +0800

    fix: update note url resolving method and 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,12 +1,30 @@
+/** @jsx jsx */
 import React from "react"
-import { graphql } from "gatsby"
+import { navigate, graphql } from "gatsby"
+import { css, jsx } from '@emotion/core'
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
-
+  if (location.pathname.match(/^\/note\//)) {
+    return (
+      <Layout location={location} title={siteTitle}>
+        <SEO title="404: Not Found" />
+        <h1>Empty Note</h1>
+        <p>There's no content in this note currently.</p>
+        <button css={{
+          border: "solid 1px #737373",
+          borderRadius: 5,
+          outline: "none",
+          cursor: "pointer",
+          fontWeight: 300,
+          backgroundColor: "transparent"
+        }} onClick={() => navigate(-1)}>Go back</button>
+      </Layout>
+    )
+  }
   return (
     <Layout location={location} title={siteTitle}>
       <SEO title="404: Not Found" />
