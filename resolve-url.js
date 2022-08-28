6ae5d0eb6ebf06f0afd4c1681f22c44ece6f3f1a
Josix <josixwang@gmail.com>
2022-08-29 00:09:11 +0800

    fix: update note url resolving method and 404 page

--- resolve-url.js
+++ resolve-url.js
@@ -1,3 +1,20 @@
+const glob = require('glob');
+
+
+
 module.exports = title => {
-  return `/note/${encodeURIComponent(title)}`
+  const filePaths = glob.sync('content/note/**/*.md',
+    {
+      ignore: [
+        'content/note/.git/**',
+        'content/_private/**',
+        'content/note/.gitignore',
+        'content/note/README.md'
+      ]
+    }).filter(file => file.match(title));
+  if (filePaths.length > 0) {
+    const parts = filePaths[0].split("/");
+    return `/${parts.slice(1, parts.length - 1).join("/")}/${encodeURIComponent(title)}`;
+  }
+  return `/note/${encodeURIComponent(title)}`;
 }
